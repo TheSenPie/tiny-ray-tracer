9c24ae57d16074c9c90bf5bd6d3a2a98fcfb79de
SenPie <smbat@duck.com>
2023-12-14 13:21:34 +0100

    Added basic anti aliasing.

--- interval.h
+++ interval.h
@@ -16,6 +16,12 @@ public:
 	bool surrouns(double x) {
 		return min < x && x < max;
 	}
+  
+  double clamp(double x) const {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+  }
 
 	static const interval empty, universe;
 };
