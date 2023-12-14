1cbb8c3b6b89c45becb928e3460cb883be758eea
SenPie <smbat@duck.com>
2023-12-14 23:05:37 +0100

    Added simple diffuse material.

--- camera.h
+++ camera.h
@@ -1,7 +1,7 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
-#include "rtweekend.h";
+#include "rtweekend.h"
 
 #include "color.h"
 #include "hittable.h"
@@ -12,7 +12,7 @@ class camera {
 public:
   double aspect_ratio{ 1.0 }; // Ratio of image width over height
   int image_width{ 100 };     // Rendered image width in pixel count
-  int samples_per_pixel = 10; // Count of random samples for each pixel
+  int samples_per_pixel{ 10 }; // Count of random samples for each pixel
   
   void render(const hittable& world) {
     initialize();
@@ -90,7 +90,8 @@ private:
   color ray_color(const ray& r, const hittable& world) {
     hit_record rec;
     if (world.hit(r, interval(0, infinity), rec)) {
-      return 0.5 * (rec.normal + color{1, 1, 1});
+      vec3 direction = random_on_hemisphere(rec.normal);
+      return 0.5 * ray_color(ray{rec.p, direction}, world);
     }
 
     vec3 unit_direction = unit_vector(r.direction());
